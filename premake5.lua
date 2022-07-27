e5e96afdec1732398b8a3c58f3c81c84144e7005
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-27 04:23:02 -0300

    Build system with premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Rengine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rengine"
+	location "Rengine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RE_PLATFORM_WINDOWS",
+			"RE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "RE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rengine/vendor/spdlog/include",
+		"Rengine/src"
+	}
+
+	links
+	{
+		"Rengine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RE_DIST"
+		optimize "On"
