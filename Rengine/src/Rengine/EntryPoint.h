86c7d5d5128c11c264577770d16373f38c48fb64
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-27 03:27:16 -0300

    Log

--- Rengine/src/Rengine/EntryPoint.h
+++ Rengine/src/Rengine/EntryPoint.h
@@ -6,6 +6,11 @@ extern Rengine::Application* Rengine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Rengine::Log::Init();
+	RE_CORE_WARN("Initialized Log!");
+	int a = 2;
+	RE_INFO("Hello! Var={0}", a);
+
 	auto app = Rengine::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Rengine/src/Rengine/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Rengine
+{
+	class RENGINE_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define RE_CORE_TRACE(...)   ::Rengine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define RE_CORE_INFO(...)    ::Rengine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define RE_CORE_WARN(...)    ::Rengine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define RE_CORE_ERROR(...)   ::Rengine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define RE_CORE_FATAL(...)   ::Rengine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define RE_TRACE(...)        ::Rengine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define RE_INFO(...)         ::Rengine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define RE_WARN(...)         ::Rengine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define RE_ERROR(...)        ::Rengine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define RE_FATAL(...)        ::Rengine::Log::GetClientLogger()->fatal(__VA_ARGS__)
