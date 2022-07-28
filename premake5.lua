bbaf6d8c5ab9f277ad50958652f68dfb7fc3ed26
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-28 17:40:45 -0300

    Fixed postbuild command

--- premake5.lua
+++ premake5.lua
@@ -73,7 +73,8 @@ project "Rengine"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+
 		}
 
 	filter "configurations:Debug"
