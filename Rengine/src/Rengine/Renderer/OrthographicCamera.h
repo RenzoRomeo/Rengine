682c3cd5a0f5589a811dc1aa4e2152f9eaf3aa1f
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-31 22:26:54 -0300

    Revert "Orthographic cammera"
    This reverts commit da7bb4498303ff3ced6f7553f9f181642799e38f.

--- Rengine/src/Rengine/Renderer/OrthographicCamera.h
+++ Rengine/src/Rengine/Renderer/OrthographicCamera.h
@@ -24,8 +24,6 @@ namespace Rengine
 		const glm::vec3& GetPosition() const { return m_Position; }
 		float GetRotation() const { return m_Rotation; }
 
-		void SetProjection(float left, float right, float bottom, float top);
-
 		const glm::mat4& GetProjectionMatrix() const { return m_ProjectionMatrix; }
 		const glm::mat4& GetViewMatrix() const { return m_ViewMatrix; }
 		const glm::mat4& GetViewProjectionMatrix() const { return m_ViewProjectionMatrix; }
